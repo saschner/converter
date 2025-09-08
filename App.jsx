36bf8dc0b44b854c7eeb924dac2071fc897654aa
Susan M Aschner, PMP <susan@alphamindsai.com>
2025-09-08 12:00:27 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import DelimiterPicker from "./components/DelimiterPicker";
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-6">LocallyConvert</h1>
+      <DelimiterPicker />
+    </div>
+  );
+}
+
+export default App;
